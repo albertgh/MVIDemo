da8c97ed997789942292346407e591fa76648e2a
ac_m1a <ac42gm@gmail.com>
2026-02-11 19:04:37 +1300

    Initial Commit

---
+++ MVIDemo/MVIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVIDemoApp.swift
+//  MVIDemo
+//
+//  Created by ac_m1a on 11/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct MVIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MVIDemoTests/MVIDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  MVIDemoTests.swift
+//  MVIDemoTests
+//
+//  Created by ac_m1a on 11/02/2026.
+//
+
+import Testing
+@testable import MVIDemo
+
+struct MVIDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
